9c7c218e14d91f515b0efd17ef61e0835155ea13
Abid Hasan <youremail@example.com>
2025-12-08 23:48:35 +0600

    added form

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            R
+            <form>
+                <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" className="input" placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-primary text-white mt-4">Login</button>
+                </fieldset>
+            </form>
         </div>
     );
 };
