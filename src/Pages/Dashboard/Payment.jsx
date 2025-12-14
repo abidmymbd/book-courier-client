67d93853b49dac9fd3ef8e5ff19ba0a0b236ef47
Abid Hasan <youremail@example.com>
2025-12-15 01:37:41 +0600

    added stripe

--- src/Pages/Dashboard/Payment.jsx
+++ src/Pages/Dashboard/Payment.jsx
@@ -16,6 +16,18 @@ const Payment = () => {
         }
     });
 
+    const handlePayment = async () => {
+        const paymentInfo = {
+            price: order.price,
+            orderId: order._id,
+            userEmail: order.userEmail,
+            bookName: order.bookName
+        }
+
+        const res = await axiosSecure.post('/create-checkout-session', paymentInfo);
+        console.log(res.data)
+    }
+
     if (isLoading) return (
         <div className="flex justify-center items-center h-full">
             <button className="btn btn-square loading"></button>
@@ -26,9 +38,11 @@ const Payment = () => {
         <div className="p-6 text-center">
             <h2 className="text-2xl text-primary font-bold mb-6">Payment</h2>
 
-            <h2 className="text-primary">Please Pay for : {order.bookName} </h2>
+            <h2 className="text-primary">Please Pay ${order.price} for : {order.bookName} </h2>
             <Link>
-                <button className='btn bg-primary  text-white font-bold hover:bg-secondary hover:text-white  transform transition duration-300 mt-5'>Pay Now</button>
+                <button
+                    onClick={handlePayment}
+                    className='btn bg-primary  text-white font-bold hover:bg-secondary hover:text-white  transform transition duration-300 mt-5'>Pay Now</button>
             </Link>
 
         </div>
